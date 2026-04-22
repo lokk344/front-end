605d830236d0504f201951dd70487425a12bfa8f
lokk344 <lokk344@gmail.com>
2026-04-22 12:53:48 +0700

    doi url

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/Homeview.vue'
 import StaticTableView from '../views/StaticView.vue'
 
 const routes = [
